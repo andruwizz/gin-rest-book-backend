777d74764c2bdeec39743523eac01396ae5da025
Arya W <andruwdev@gmail.com>
2026-08-25 15:42:06 +0700

    docs: setup swaggo api docs

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	_ "github.com/andruwizz/gin-book-sharing-backend/docs"
 	"github.com/andruwizz/gin-book-sharing-backend/internal/config"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
+// @title Book Sharing API
+// @version 1.0
+// @description This is a Book Sharing API
+
+// @license.name MIT
+// @license.url https://mit-license.org/
+
+// @host localhost:3000
+// @BasePath /api/v1
 func main() {
 	// Init Database
 	db := config.NewDatabase()
@@ -15,6 +27,8 @@ func main() {
 		DB:         db,
 		RouteGroup: app.Group("/api/v1"),
 	})
+
+	app.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	app.Run(":3000")
 }
