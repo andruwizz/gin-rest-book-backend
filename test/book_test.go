4780e09e719a82847b46dad14730f7aa27cee0b3
Arya W <andruwdev@gmail.com>
2026-09-02 11:11:31 +0700

    test: add response data assert to book list api test

--- test/book_test.go
+++ test/book_test.go
@@ -10,14 +10,19 @@ import (
 )
 
 func TestListBookUnauthorized(t *testing.T) {
+	// Arrange
 	app := config.NewGin()
 	config.Bootstrap(&config.BootstrapConfig{
 		App: app,
 	})
 
+	// Act
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest(http.MethodGet, "/api/v1/books/", nil)
 	app.ServeHTTP(w, req)
 
+	// Assert
+	res := `{"success":false,"error":{"message":"authentication token not found"}}`
 	assert.Equal(t, http.StatusUnauthorized, w.Code)
+	assert.Equal(t, res, w.Body.String())
 }
