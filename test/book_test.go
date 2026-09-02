e3382fffe1bedcdc749cf9bb6933bdedcb8a89ab
Arya W <andruwdev@gmail.com>
2026-09-02 16:39:18 +0700

    test: add book repository mock

--- test/book_test.go
+++ test/book_test.go
@@ -3,18 +3,35 @@ package test
 import (
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 
-	"github.com/andruwizz/gin-rest-book-backend/internal/config"
+	"github.com/andruwizz/gin-rest-book-backend/internal/delivery/handler"
+	"github.com/andruwizz/gin-rest-book-backend/internal/delivery/routes"
+	"github.com/andruwizz/gin-rest-book-backend/internal/repository/mock"
+	"github.com/andruwizz/gin-rest-book-backend/internal/usecase"
+	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 )
 
+func TestMain(m *testing.M) {
+	gin.SetMode(gin.TestMode)
+	os.Exit(m.Run())
+}
+
+func NewMockSetup() *handler.BookHandler {
+	bookRepository := mock.NewMockBookRepository()
+	bookService := usecase.NewBookUsecase(bookRepository)
+	bookHandler := handler.NewBookHandler(bookService)
+
+	return bookHandler
+}
+
 func TestListBookUnauthorized(t *testing.T) {
 	// Arrange
-	app := config.NewGin()
-	config.Bootstrap(&config.BootstrapConfig{
-		App: app,
-	})
+	app := gin.Default()
+	rg := app.Group("/api/v1")
+	routes.BookRouter(rg, NewMockSetup())
 
 	// Act
 	w := httptest.NewRecorder()
